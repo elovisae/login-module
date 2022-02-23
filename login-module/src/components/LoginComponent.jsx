fa092660e08949dcf8f51a9d23b9aaa6bbab8768
Lovisa Eriksson <lovisa@ahna.se>
2022-02-23 12:11:43 +0100

    added css and all elements

--- login-module/src/components/LoginComponent.jsx
+++ login-module/src/components/LoginComponent.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
+import './styles/loginComponent.css'
 
 export class LoginComponent extends Component {
   render() {
     return (
-      <div>LoginComponent</div>
+      <div>
+          <h1>Login to Your Gallery</h1>
+          <article> 
+              <form action="">
+                  <div className="form-group">
+                    <label htmlFor="email">Mail: </label>
+                    <input type="email" name="email" id="email"/>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" name="password" id="password" />
+                  </div>
+                  <button>Sign in</button>
+
+              </form>
+              <section id="register">
+              <p>Don't have an account?</p>
+              <button>Register now!</button>
+                  
+              </section>
+          </article>
+      </div>
     )
   }
 }
