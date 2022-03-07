1550c85f18352b1ee31356448f0aa1c327f170ed
Lovisa Eriksson <lovisa@ahna.se>
2022-03-07 09:23:26 +0100

    installed react-router-dom

--- login-module/src/App.js
+++ login-module/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import LoginComponent from './components/LoginComponent';
 import RegisterComponent from './components/RegisterComponent';
 
 function App() {
   return (
     <div className="App">
-      <RegisterComponent />
+      <LoginComponent />
     </div>
   );
 }
