d96f24d35239806a5f2da42f054e073884b6dca7
Lovisa Eriksson <lovisa@ahna.se>
2022-03-16 20:53:57 +0100

    must be logged in to see /gallery + is personified

--- login-module/src/components/Gallery.jsx
+++ login-module/src/components/Gallery.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {useLocation, useNavigate, Link} from 'react-router-dom'
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
+  const location  = useLocation();
+  const navigate  = useNavigate();
+
+  if(location.state && location.state.loggedIn){
+    return (
+      <div>
+        <h1>Welcome {location.state.name}!</h1>
+      </div>
+    )
+
+  }else{
+    return(
+      <div>
+        <h1>Oops!</h1>
+        <p>You are not logged in! Please visit <Link to='/'>this page</Link> and login before trying to reach this page </p>
+      </div>
+    )
+  }
 }
 
 export default Gallery
